ea01d4ef78276ef059bac0882a4aee0a7ad8a0a8
myungwan-git <nsubway1070635@gmail.com>
2021-07-26 19:59:51 +0900

    20210726-JPA-domain

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello"; 
     }
 
     @GetMapping("/hello/dto")

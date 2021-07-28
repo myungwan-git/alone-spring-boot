5f18ba06728c55db29cbb4d1dd3174833f70a67a
myungwan-git <nsubway1070635@gmail.com>
2021-07-28 20:55:18 +0900

    20210728-JPA-domain

---
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.jojoldu.book.springboot.service;
+
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequsetDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequsetDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
